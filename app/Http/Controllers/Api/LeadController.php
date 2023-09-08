ba16b11a27ce391bc0684062a38683289bc696d0
Anieb22 <anielloverdino@gmail.com>
2023-09-08 16:32:33 +0200

    aggiunte validation

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -4,12 +4,24 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class LeadController extends Controller
 {
     public function store(Request $request){
         $data = $request->all();
 
-        
+        $validator = Validator::make($data, [
+            'name'=>'required',
+            'email'=>'required|email',
+            'message'=>'nullable'
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'success' =>false,
+                'errors' => $validator->errors()
+            ]);
+        }
     }
 }
