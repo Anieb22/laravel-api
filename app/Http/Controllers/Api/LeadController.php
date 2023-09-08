583aa1f49e3083ea2ac2a82a5f22ae0c4cd372d6
Anieb22 <anielloverdino@gmail.com>
2023-09-08 16:25:33 +0200

    creata migration e controller per le mail

---
+++ app/Http/Controllers/Api/LeadController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class LeadController extends Controller
+{
+    public function store(Request $request){
+        $data = $request->all();
+
+        
+    }
+}
---
+++ resources/views/mails/new_email_contact.blade.php
@@ -0,0 +1,9 @@
+<h1>HELLOOOOOOOOOOOO</h1>
+<p>is anyone there
+    utente: {{ $lead->name}} {{$lead->surname}}
+    <br>
+    Email: {{$lead->email}}
+    <br>
+    Contenuto <br>
+    {{$lead->content}}
+</p>
